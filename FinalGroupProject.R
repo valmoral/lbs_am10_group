b17475a5ea29e3fe57dc01b56e69d03a75a77537
Lazar Jelić <lazarjelic98@gmail.com>
2021-11-12 16:12:05 +0000

    Added map

--- FinalGroupProject.R
+++ FinalGroupProject.R
@@ -22,6 +22,15 @@ restaurants <- read_csv('data/FastFoodRestaurants.csv') %>%
   filter(name %in% unique(calories$restaurant)) %>%
   select(-address, -country, -keys, -postal_code, -province, -websites)
 
+abbrevs <- read_csv('data/state-abbrevs.csv') %>%
+  clean_names()
+
+population <- read_csv('data/us_pop.csv') %>%
+  clean_names() %>%
+  left_join(abbrevs, by='state') %>%
+  select(-state) %>%
+  rename(state=abbreviation)
+
 # Q2
 calories %>%
   ggplot() +
@@ -33,12 +42,18 @@ calories_per_restaurant <- calories %>%
   group_by(restaurant) %>%
   summarise(median_calories=median(calories))
 
-calories_per_restaurant <- restaurants %>%
+restaurant_with_calories <- restaurants %>%
   inner_join(calories_per_restaurant, by=c('name'='restaurant')) %>%
-  group_by(state, name) %>%
-  summarise(total_calories=n()*median_calories)
-  
+  group_by(state, median_calories) %>%
+  count() %>%
+  mutate(total_calories=n*median_calories) %>%
+  group_by(state) %>%
+  summarise(total_calories=sum(total_calories)) %>%
+  inner_join(population, by='state') %>%
+  mutate(total_calories=total_calories/population) %>%
+  select(-population)
+
 get_urbn_map('states', sf=T) %>%
-  inner_join(calories_per_restaurant, by=c('state_abbv'='state')) %>%
+  inner_join(restaurant_with_calories, by=c('state_abbv'='state')) %>%
   ggplot(aes(fill=total_calories)) +
-  geom_sf()
+  geom_sf(colour='#ffffff')
